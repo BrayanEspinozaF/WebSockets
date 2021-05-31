103223447bb1a2c3d7158b28bc8e9f864bfc0d1a
Brayan <bryan.espinoza97@hotmail.com>
2021-05-31 10:26:59 -0600

    socket.io para el frontend

--- servers/main.js
+++ servers/main.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var app = express();
-
 var server = require('http').Server(app);
-
 var io = require('socket.io')(server);
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
     res.status(200).send('hola mundo');
+});
+
+io.on('connection', function(socket) {
+    console.log('alguin se ha conectado con socekt')
 });
 
 server.listen(3002, function(){
