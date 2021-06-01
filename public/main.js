354866b3144a022afea3ccc9bb1d3e14145d438d
Brayan <bryan.espinoza97@hotmail.com>
2021-06-01 15:56:19 -0600

    creacion del manejo de eventos desde el cliente hasta el servidor

--- public/main.js
+++ public/main.js
@@ -1,4 +1,7 @@
 //variable que permite al frontend conectarse al backend
 var socket = io.connect(`http://localhost:3002`, {'forceNew':true});
 
-//manda al servidor un mensaje de connect y aparece en console.log
+//el cliente maneja datos mediante mensajes, esto se llama eventos y se mostraran por la consola del navegador
+socket.on('messages', function(data){
+    console.log(data);
+});
